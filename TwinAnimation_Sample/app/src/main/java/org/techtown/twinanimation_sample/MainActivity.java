db0288c605503489fd60e53532dab2d4ff8ba764
HwangGyuBin <tkrlghfh2@naver.com>
2021-02-12 17:47:13 +0900

    TwinAnimation Sample Code

---
+++ TwinAnimation_Sample/app/src/main/java/org/techtown/twinanimation_sample/MainActivity.java
@@ -0,0 +1,36 @@
+package org.techtown.twinanimation_sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale);
+                v.startAnimation(anim);
+            }
+        });
+
+        Button btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale2);
+                v.startAnimation(anim);
+            }
+        });
+    }
+}
